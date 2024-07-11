dc9262a0b5959dea6d020e0d70e57eb16a5c90f1
Greg Herlein <gherlein@herlein.com>
2024-07-11 12:06:04 -0700

    added vscode

--- lwipopts.h
+++ lwipopts.h
@@ -3,4 +3,12 @@
 
 #include "lwipopts_common.h"
 
+#define LWIP_HTTPD 1
+// #define LWIP_HTTPD_SSI 1
+// #define LWIP_HTTPD_CGI 1
+//  don't include the tag comment - less work for the CPU, but may be harder to
+//  debug
+// #define LWIP_HTTPD_SSI_INCLUDE_TAG 0
+//  use generated fsdata
+#define HTTPD_FSDATA_FILE "fsdata.c"
 #endif
